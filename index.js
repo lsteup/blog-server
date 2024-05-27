ac504a43dc15c2b889bcb52716ed6f425d78061c
lsteup <loras12@gmail.com>
2024-05-27 13:38:17 +0200

    add swagger ui

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+//swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 //connect to monggose
 const connectDB = require("./db/connect");
 
@@ -23,6 +28,8 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.send('<h1>Blog API</h1><a href="/api-docs">Documentation</a>');

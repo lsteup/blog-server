4806e3c0ae9e856d0a5df4fe10a0c199287471f8
lsteup <loras12@gmail.com>
2024-05-27 12:50:58 +0200

    add security packages

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+//security
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 //connect to monggose
 const connectDB = require("./db/connect");
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 app.get("/", (req, res) => {
   res.send('<h1>Blog API</h1><a href="/api-docs">Documentation</a>');

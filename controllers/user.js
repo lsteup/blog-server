814a686612a193c6fdfe31670297c69b14d16198
lsteup <loras12@gmail.com>
2024-05-31 13:29:47 +0200

    populate activity in get user response

--- controllers/user.js
+++ controllers/user.js
@@ -3,23 +3,20 @@ const User = require("../models/user");
 const getUser = async (req, res) => {
   const { id } = req.params;
   try {
-    const user = await User.findOne({ _id: id });
-    console.log(user);
+    const user = await User.findOne({ _id: id }).populate("activity").exec();
+    res.json({ data: user });
   } catch (error) {
     res.json(error);
   }
-  res.send("get user");
 };
 
 const getUsers = async (req, res) => {
   try {
     const users = await User.find({});
-    console.log(users);
+    res.json({ data: users });
   } catch (err) {
     res.json(err);
   }
-
-  res.send("get users");
 };
 
 module.exports = { getUser, getUsers };

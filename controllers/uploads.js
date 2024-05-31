8c4ecf641588502d1bb131abc5de652b21390cbc
lsteup <loras12@gmail.com>
2024-05-31 11:06:27 +0200

    add errors to upload controller

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,8 +1,39 @@
 const { StatusCodes } = require("http-status-codes");
+const path = require("path");
+const { BadRequestError } = require("../errors/index");
 
 const uploadPostImage = async (req, res) => {
-  console.log(req.files);
-  res.send("upload post image");
+  if (!req.files) {
+    throw new BadRequestError("No file uploaded");
+  }
+  console.log(req.files.image);
+  const postImg = req.files.image;
+  console.log(postImg);
+
+  if (!postImg.mimetype.startsWith("image")) {
+    throw new BadRequestError("Please upload image");
+  }
+
+  const maxSize = 1024 * 1024;
+
+  if (postImg.size.maxSize) {
+    throw new BadRequestError("Please upload image smaller than 1MB");
+  }
+
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads/" + `${postImg.name}`
+  );
+
+  console.log(imagePath);
+  try {
+    await postImg.mv(imagePath);
+    res
+      .status(StatusCodes.OK)
+      .json({ image: { src: `/uploads/${postImg.name}` } });
+  } catch (error) {
+    res.json({ msg: error });
+  }
 };
 
 module.exports = { uploadPostImage };
--- index.js
+++ index.js
@@ -26,6 +26,8 @@ app.use(
     max: 100, // limit each IP to 100 requests per windowMs
   })
 );
+
+app.use(express.static("./public"));
 app.use(express.json());
 app.use(fileUpload());
 app.use(helmet());
